aef278761f959305b494582efc091f1643b6e361
Jay Thompson <jay_thompson@wgbh.org>
2019-04-08 08:39:17 -0500

    Getting ready for tests

--- setup.py
+++ setup.py
@@ -14,15 +14,15 @@ def get_version(*file_paths):
     """Retrieves the version from hueydrfapi/__init__.py"""
     filename = os.path.join(os.path.dirname(__file__), *file_paths)
     version_file = open(filename).read()
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              version_file, re.M)
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M
+    )
     if version_match:
         return version_match.group(1)
     raise RuntimeError('Unable to find version string.')
 
 
 version = get_version("hueydrfapi", "__init__.py")
-
 
 if sys.argv[-1] == 'publish':
     try:
@@ -66,11 +66,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
